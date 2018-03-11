bdc28a76cc46a91ac603f7b256ff6032ac18eed6
Felix Nagel <info@felixnagel.com>
2018-03-11 12:37:11 +0100

    Use environment variables in TYPO3 introduction package install sh file

--- demo/install.sh
+++ demo/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 php vendor/bin/typo3cms install:setup --force --no-interaction --skip-integrity-check \
-    --database-name="dbname"  --database-host-name="db" --database-port="3306" \
-    --database-user-name="dbuser" --database-user-password="dbpass" --use-existing-database \
-    --admin-user-name="admin" --admin-password="password" \
+    --database-name="$MYSQL_DATABASE"  --database-host-name="db" --database-port="3306" \
+    --database-user-name="$MYSQL_USER" --database-user-password="$MYSQL_PASSWORD" --use-existing-database \
+    --admin-user-name="$TYPO3_USER" --admin-password="$TYPO3_PASSWORD" \
     --site-name="TYPO3 Introduction Package" --site-setup-type="no"
 
 # Needed because even if this script is run as www-data the files / folders created
