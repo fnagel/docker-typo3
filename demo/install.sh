9cbe3181d1c12b5e097eedf2c362bf5d41984ee0
Felix Nagel <info@felixnagel.com>
2020-06-06 23:25:20 +0200

    Update demo for TYPO3 10.4 and add /var path for newer TYPO3 versions

--- demo/install.sh
+++ demo/install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+chown -R www-data:www-data /var/www/project
+
+touch web/FIRST_INSTALL
+touch web/typo3conf/ENABLE_INSTALL_TOOL
+
 php vendor/bin/typo3cms install:setup --force --no-interaction --skip-integrity-check \
-    --database-name="$MYSQL_DATABASE"  --database-host-name="db" --database-port="3306" \
-    --database-user-name="$MYSQL_USER" --database-user-password="$MYSQL_PASSWORD" --use-existing-database \
+    --database-name="$MYSQL_DATABASE" --use-existing-database \
+    --database-host-name="db" --database-port="3306" \
+    --database-user-name="$MYSQL_USER" --database-user-password="$MYSQL_PASSWORD" \
     --admin-user-name="$TYPO3_USER" --admin-password="$TYPO3_PASSWORD" \
-    --site-name="TYPO3 Introduction Package" --site-setup-type="no"
+    --site-name="TYPO3 Introduction Package" \
+    --site-setup-type="site" --site-base-url="http://$APACHE_DOMAIN" \
+    --web-server-config="apache"
 
-# Needed because even if this script is run as www-data the files / folders created
-# in fileadmin have root permissions
-# @todo Fix this
-chown -R www-data:www-data /var/www/project/fileadmin
-
-chown www-data:www-data /var/www/html/web/typo3conf/LocalConfiguration.php
+# Reuse site configuration created by typo3 console for site tree created by introduction package
+sed -ri -e 's!rootPageId: 1!rootPageId: 2!g' /var/www/html/config/sites/main/config.yaml
+php vendor/bin/typo3cms cache:flush
